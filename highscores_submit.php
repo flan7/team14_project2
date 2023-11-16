a28588e46798c9fe9da81d654935ce31315904ff
flan <flan@arch.fyi>
2023-11-16 17:04:14 -0500

    pics/fixed register

--- highscores_submit.php
+++ highscores_submit.php
@@ -2,8 +2,6 @@
   $newline = "\n".$_SESSION["final_score"].",".$_SESSION["username"];
   file_put_contents('highscores.txt', $newline, FILE_APPEND | LOCK_EX);
 
-  //redirect page depending on output
   header("location: highscores.php");
 
-header("location: index.html");
 ?>
--- register_submit.php
+++ register_submit.php
@@ -1,4 +1,4 @@
-<?php
+<?php session_start();
 if(isset($_POST["username"]) && isset($_POST["password"])){
   $newline = "\n".$_POST["username"].",".$_POST["password"];
   file_put_contents('users.txt', $newline, FILE_APPEND | LOCK_EX);
